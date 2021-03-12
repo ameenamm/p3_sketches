f7644541890020145e512434f354282e400f011f
ameenamm <ameenammuhammad@gmail.com>
2021-03-12 02:27:44 -0600

    changes

---
+++ sketch.js
@@ -0,0 +1,51 @@
+function setup() { 
+var c = createCanvas(1000,1000); //width and height in pixels
+
+//rect(0,0,1000,1000)
+
+stroke(0)
+//no fill
+for(var i = 0;i<100; i++)  {
+ 
+ellipse(i*50,i*50,250,250)
+
+
+}
+fill(0)
+//stroke(255)
+noStroke(255)
+
+rect(100,100,100,500)
+ellipse(260,230,250,250)
+fill(255)
+ellipse(260,230,75,75)
+
+}
+
+function draw(){
+
+
+}
+
+
+
+
+//var i = 0;
+//while(i<10) {
+	//add code here
+
+	//ellipse(10*i,10*i,250,250)
+
+	//i=i+i;
+
+
+
+
+
+
+
+
+
+
+
+//}
